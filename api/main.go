770bdc1d46abc6245197e4aa24cc9abb73e68ab7
Joel Saunders <joel.saunders@complyadvantage.com>
2019-10-04 18:35:25 +0100

    add default data and rename config

--- api/main.go
+++ api/main.go
@@ -1,17 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/joelsaunders/blog-go/api/pkg/api"
 	config "github.com/joelsaunders/blog-go/api/pkg/config"
+	"github.com/joelsaunders/blog-go/api/pkg/models"
+	"github.com/joelsaunders/blog-go/api/pkg/repository/post"
 	"github.com/joelsaunders/blog-go/api/pkg/repository/postgres"
+	"github.com/joelsaunders/blog-go/api/pkg/repository/user"
 
 	"github.com/go-chi/chi"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
+
+func testDataSetup(db *sqlx.DB) {
+	ctx := context.Background()
+
+	newUser := &models.NewUser{
+		Email:    "joel.st.saunders@gmail.com",
+		Password: "password",
+	}
+	userStore := &user.PGUserStore{DB: db}
+
+	user, err := userStore.Create(ctx, newUser)
+
+	if err != nil {
+		log.Println("user could not be created")
+	}
+
+	testPost := models.Post{
+		Created:     time.Now().Round(time.Second).UTC(),
+		Modified:    time.Now().Round(time.Second).UTC(),
+		Slug:        "test slug",
+		Title:       "test title",
+		Body:        "test body",
+		Picture:     "test picture",
+		Description: "test description",
+		Published:   true,
+		AuthorID:    user.ID,
+	}
+
+	postStore := post.PGPostStore{DB: db}
+
+	_, err = postStore.Create(ctx, &testPost)
+
+	if err != nil {
+		log.Panicln("could not create default post")
+	}
+}
 
 func main() {
 	configuration, err := config.NewConfig()
@@ -33,6 +75,8 @@ func main() {
 	}
 
 	router := api.Routes(configuration, db)
+
+	testDataSetup(db)
 
 	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
 		log.Printf("%s %s\n", method, route) // Walk and print out all routes
